49a4f391496f0ea27db7bff1406e0f7c24caf2b0
PRASANTH P.C <prasanth.pc@fingent.com>
2017-02-14 17:57:16 +0530

    second commit

--- whozTheCaller/AppDelegate.swift
+++ whozTheCaller/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         print("hello")
+        
+        
+        print("second push")
         // Override point for customization after application launch.
         return true
     }
